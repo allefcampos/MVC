e2024ef001ab8138fc41bd1f10675b36a577dcf5
Allef Campos <allef-cleiton88@hotmail.com>
2022-10-17 21:41:29 -0300

    Update Project

--- 01 - DemoMVC/Controllers/HomeController.cs
+++ 01 - DemoMVC/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
 ﻿using DemoMVC.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DemoMVC.Controllers
 {
@@ -29,6 +25,7 @@ namespace DemoMVC.Controllers
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [Route("error-default")]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
