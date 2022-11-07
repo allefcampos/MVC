5819a3a738ef973138dff951bf450a172ebeb3f2
allefcampos <allef-cleiton88@hotmail.com>
2022-11-07 20:09:27 -0300

    Criação da estrutura de um novo projeto

---
+++ AppModelo/src/DevIO.UI.Site/Program.cs
@@ -0,0 +1,29 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+#region " Configurando serviços no container "
+
+// Adicionando MVC no pipeline
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+#endregion
+
+# region " Configurando o resquest dos serviços no pipeline "
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseStaticFiles();
+
+// Adicionando suporte a rotas
+app.UseRouting();
+
+// Rota padrão
+app.MapControllerRoute("default","{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+#endregion
